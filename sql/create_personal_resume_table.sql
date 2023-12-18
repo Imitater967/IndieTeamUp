fdff5e4573e607c6d14e2d6d5bf1046142ed7f71
Megumin <megumin_lyq@outlook.com>
2023-12-18 15:03:15 +0800

    完成个人简历

---
+++ sql/create_personal_resume_table.sql
@@ -0,0 +1,52 @@
+create table `itu_user_person_resume`
+(
+    `uuid`         int not null primary key ,
+    `work_type` ENUM('线上','线下','线下或线下') not null,
+    `sex` ENUM('男','女'),
+    'pay_method' ENUM('月薪','外包','均可'),
+    'min_qualify' ENUM('无','个体户','小规模','一般'),
+    'current_province' smallint,
+    'expect_province' smallint,
+    'first_job' varchar(8),
+    'second_job' varchar(8),
+    'salary_stock' tinyint,
+    'salary' tinyint,
+    'introduce' varchar(255),
+    primary key (`uuid`)
+) engine = InnoDB
+  default charset = utf8mb4;
+
+create table `itu_user_person_education`
+(
+    `uuid`         int not null primary key ,
+    'degree'    enum('大专','本科','硕士及以上') not null ,
+    'university' varchar(16) not null ,
+    'graduate' DATETIME not null ,
+    primary key (`uuid`)
+) engine = InnoDB
+  default charset = utf8mb4;
+
+
+create table `itu_user_person_skill_overview`
+(
+    `uuid`         int not null primary key ,
+    'skill_name' varchar(16) not null ,
+    'skill_level' tinyint not null ,
+    primary key (`uuid`)
+) engine = InnoDB
+  default charset = utf8mb4;
+
+
+create table `itu_user_person_skill_detail`
+(
+    `uuid`         int not null primary key ,
+    'skill_name' varchar(16) not null primary key ,
+    'skill_level' tinyint not null ,
+    'start_time' DATETIME not null ,
+    'end_time' DATETIME not null ,
+    'company' varchar(50) not null ,
+    'project' varchar(16) not null ,
+    'url'   varchar(255) not null ,
+    primary key (`uuid`)
+) engine = InnoDB
+  default charset = utf8mb4;
--- sql/create_user_table.sql
+++ sql/create_user_table.sql
@@ -1,8 +1,19 @@
-create table `user`
+create table `itu_user`
 (
-    `id`         int unsigned auto_increment,
+    `uuid`         int unsigned auto_increment,
     `username` varchar(16) not null,
-    `password` varchar(32) not null,
-    primary key (`id`)
+    `password` varchar(32),
+    'phone_code' smallint,
+    'jwt_code' varchar(255),
+    primary key (`uuid`)
 ) engine = InnoDB
   default charset = utf8mb4;
+create table 'itu_user_identify'(
+    'uuid'  int not null,
+    'id_card' varchar(18),
+    'biz_cert' varchar(18),
+    'wx_openid' varchar(255),
+    'qq_openid' varchar(255),
+    primary key ('uuid')
+
+) engine = InnoDB  default charset = utf8mb4;
