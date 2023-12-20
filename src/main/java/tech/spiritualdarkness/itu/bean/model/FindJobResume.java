7505878c023094c819929360c669cab3a881f9df
Megumin <megumin_lyq@outlook.com>
2023-12-20 20:12:07 +0800

    加了一点东西

--- src/main/java/tech/spiritualdarkness/itu/bean/model/FindJobResume.java
+++ src/main/java/tech/spiritualdarkness/itu/bean/model/FindJobResume.java
@@ -1,15 +1,37 @@
 package tech.spiritualdarkness.itu.bean.model;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 import tech.spiritualdarkness.itu.bean.enumtype.*;
 
 @Data
 public class FindJobResume {
+    @NotEmpty
+    private Integer uuid;
+    @NotEmpty
     private String real_name;
+    @NotEmpty
     private int age;
+    @NotEmpty
     private WorkType work_type;
+    @NotEmpty
     private Sex sex;
+    @NotEmpty
     private PayMethod pay_method;
+    @NotEmpty
     private WorkMethod work_method;
+    @NotEmpty
     private Qualify min_qualify;
+    private String current_province;
+    private String expect_province;
+    @NotEmpty
+    private String first_job;
+    private String second_job;
+    @NotEmpty
+    private int salary_stock;
+    @NotEmpty
+    private int salary;
+    @NotEmpty
+    private String introduce;
+
 }
