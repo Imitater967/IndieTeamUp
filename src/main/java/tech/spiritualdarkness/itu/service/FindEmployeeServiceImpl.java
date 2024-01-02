146f2bdad50f80f03619ab202b33070ac0d3970d
林雨晴 <13719733+MGMLinYuQing@user.noreply.gitee.com>
2024-01-02 18:54:17 +0800

    完成progress

--- src/main/java/tech/spiritualdarkness/itu/service/FindEmployeeServiceImpl.java
+++ src/main/java/tech/spiritualdarkness/itu/service/FindEmployeeServiceImpl.java
@@ -1,10 +1,14 @@
 package tech.spiritualdarkness.itu.service;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import tech.spiritualdarkness.itu.bean.po.FindEmployeeResume;
+import tech.spiritualdarkness.itu.bean.po.ProjectProgress;
 import tech.spiritualdarkness.itu.dao.FindEmployeeResumeMapper;
 import tech.spiritualdarkness.itu.bean.vo.response.Result;
 import tech.spiritualdarkness.itu.bean.vo.response.ResumeStatus;
+import tech.spiritualdarkness.itu.dao.ProjectProgressMapper;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -12,6 +16,8 @@ import java.util.Map;
 @Service
 public class FindEmployeeServiceImpl implements  IFindEmployeeService{
     final FindEmployeeResumeMapper resumeMapper;
+    @Autowired
+    ProjectProgressMapper progressMapper;
 
     public FindEmployeeServiceImpl(FindEmployeeResumeMapper resumeMapper) {
         this.resumeMapper = resumeMapper;
@@ -20,6 +26,7 @@ public class FindEmployeeServiceImpl implements  IFindEmployeeService{
     //根据ProjectId筛选
     @Override
     public Result<FindEmployeeResume, ResumeStatus> getById(Integer uuid) {
+        //查找基础简历
         Result<FindEmployeeResume,ResumeStatus> result=new Result<>();
         var resume = resumeMapper.selectById(uuid);
         if(resume == null){
@@ -28,6 +35,12 @@ public class FindEmployeeServiceImpl implements  IFindEmployeeService{
         }
         result.setStatus(ResumeStatus.ResumeFound);
         result.setData(resume);
+
+        //链表查询
+        QueryWrapper skillQuery = new QueryWrapper();
+        skillQuery.eq("project_id",uuid);
+        resume.setProgress(progressMapper.selectList(skillQuery));
+
         return result;
     }
 
@@ -37,7 +50,7 @@ public class FindEmployeeServiceImpl implements  IFindEmployeeService{
         try{
             resumeMapper.deleteById(findJobResume.getPublisher_uuid());
             resumeMapper.insert(findJobResume);
-
+            updateProjectProgress(findJobResume);
             result.setData(findJobResume);
             result.setStatus(ResumeStatus.ResumeUpdated);
             return result;
@@ -50,12 +63,23 @@ public class FindEmployeeServiceImpl implements  IFindEmployeeService{
 
     @Override
     public Result<FindEmployeeResume[], ResumeStatus> getByUploaderId(Integer uuid) {
-        Result<FindEmployeeResume[],ResumeStatus> result=new Result<>();
-        Map<String,Object> columns = new HashMap<>();
-        columns.put("publisher_uuid",uuid);
-        var resultValues = resumeMapper.selectByMap(columns);
-        result.setData((FindEmployeeResume[]) resultValues.toArray());
-        result.setStatus(ResumeStatus.ResumeFound);
+//        Result<FindEmployeeResume[],ResumeStatus> result=new Result<>();
+//        Map<String,Object> columns = new HashMap<>();
+//        columns.put("publisher_uuid",uuid);
+//        var resultValues = resumeMapper.selectByMap(columns);
+//        result.setData((FindEmployeeResume[]) resultValues.toArray());
+//        result.setStatus(ResumeStatus.ResumeFound);
         return null;
+    }
+
+    public void updateProjectProgress(FindEmployeeResume resume){
+        var uuid = resume.getPublisher_uuid();
+        var progresses = resume.getProgress();
+
+        progressMapper.deleteById(uuid);
+
+        for (ProjectProgress progress : progresses) {
+            progressMapper.insert(progress);
+        }
     }
 }
